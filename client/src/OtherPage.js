f3d19b02aefd9f7e5803e15b59a969d0906f435c
vamayar <malkavian_va@hotmail.com>
2019-10-25 22:54:27 -0500

    first commit

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export default () =>{
-    return (
-        <div>
-            Im som other oage!
-            <Link to="/"> Go back home</Link>
-        </div>
-    );
+export default () => {
+  return (
+    <div>
+      Im some other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
 };
